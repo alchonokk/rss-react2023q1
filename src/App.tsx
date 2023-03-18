16287158d24033246a0d03e99abd99b113b554ee
alchonokk <galunia_suglob@mail.ru>
2023-03-18 17:44:47 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import LayoutApp from 'components/LayoutApp';
+import { ROUTES } from 'constants/appConstants';
+import { AboutUsPage } from 'pages/AboutUsPage';
+import { ErrorPage } from 'pages/ErrorPage';
+import { MainPage } from 'pages/MainPage';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path={ROUTES.MAIN} element={<LayoutApp />}>
+        <Route index element={<MainPage />} />
+        <Route path={ROUTES.ABOUT_US} element={<AboutUsPage />} />
+      </Route>
+      <Route path="*" element={<LayoutApp />}>
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
