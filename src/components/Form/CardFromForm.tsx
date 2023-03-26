e6427f9728cf3247f07c74b0b1914b2bb861dedc
alchonokk <galunia_suglob@mail.ru>
2023-03-26 19:48:06 +0300

    feat: add Card for form

--- src/components/Form/CardFromForm.tsx
+++ src/components/Form/CardFromForm.tsx
@@ -7,17 +7,21 @@ class CardForm extends React.Component<FormData> {
   }
   render() {
     return (
-      <div className="card" data-testid="form-card">
+      <div className="card card-form" data-testid="form-card">
         <img data-testid="cardImage" src={this.props.file} alt="img picture" />
-        <h3>{this.props.name}</h3>
-        <h3>{this.props.surName}</h3>
-        {this.props.switcher ? (
-          <p data-testid="gender">female</p>
+        <h5 className="card-detail">Name: {this.props.name}</h5>
+        <h5 className="card-detail">SurName: {this.props.surName}</h5>
+        {this.props.female ? (
+          <p data-testid="gender" className="card-detail">
+            Gender: female
+          </p>
         ) : (
-          <p data-testid="gender">male</p>
+          <p className="card-detail" data-testid="gender">
+            Gender: male
+          </p>
         )}
-        <p>Дата: {this.props.date}</p>
-        <h4>{this.props.city}</h4>
+        <p className="card-detail">Date: {this.props.date}</p>
+        <h5 className="card-detail">City: {this.props.city}</h5>
       </div>
     );
   }
