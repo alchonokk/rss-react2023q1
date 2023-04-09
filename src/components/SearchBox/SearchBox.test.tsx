19c5a8121b0d93f4a005f78e1d7e195e8dadae9b
alchonokk <galunia_suglob@mail.ru>
2023-04-09 18:25:07 +0300

    feat: add tests

--- src/components/SearchBox/SearchBox.test.tsx
+++ src/components/SearchBox/SearchBox.test.tsx
@@ -1,40 +1,70 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { SearchBox } from '.';
+import axios from 'axios';
+jest.mock('axios');
 
 describe('inputBox', () => {
-  let unmount: () => void;
-  beforeEach(() => {
+  beforeEach(async () => {
     Object.defineProperty(window, 'localStorage', {
       value: {
-        getItem: jest.fn(),
-        setItem: jest.fn(),
+        getItem: jest.fn(() => null),
+        setItem: jest.fn(() => null),
       },
       writable: true,
     });
     const renderResult = render(<SearchBox />);
-    unmount = renderResult.unmount;
+    renderResult;
   });
 
-  test('input', () => {
-    const inputElement = screen.getByPlaceholderText(/Search/i);
-    expect(inputElement).toBeInTheDocument();
-  });
-
-  describe('LocalStorage dataInputBox', () => {
-    test('localInput Event', () => {
-      const inputElement = screen.getByPlaceholderText(/Search/i);
-      act(() => {
-        userEvent.type(inputElement, 'testing');
-      });
-      unmount();
-      expect(window.localStorage.setItem).toHaveBeenCalledWith('searchData', 'testing');
+  test('localStorage after submit', async () => {
+    const inputElement = screen.getByTestId('input');
+    await act(() => {
+      userEvent.type(inputElement, 'testing');
     });
+    await act(() => {
+      fireEvent.submit(screen.getByTestId('input-submit'));
+    });
+    expect(window.localStorage.setItem).toHaveBeenCalledWith('searchData', 'testing');
+  });
+});
 
-    test('localInput unmount', () => {
-      unmount();
-      expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
+const articles = [
+  {
+    author: 'Remy Tumin',
+    content: 'Kanarek ',
+    description: 'Michael Barisone, an Olympic equestrian coach',
+    publishedAt: '2023-04-03T22:04:40Z',
+    source: { id: 'ff', name: 'New York Times' },
+    title: 'Dressage Coach Acquitted of Shooting Student by Reason of Insanity',
+    url: 'https://www.nytimes.com/ty.html',
+    urlToImage: 'https://000static01.nyt.com/images/',
+  },
+  {
+    author: 'Remy',
+    content: 'Ka ',
+    description: 'Michael',
+    publishedAt: '2023-04-03T22:04:40Z',
+    source: { id: 'gg', name: 'New York Times' },
+    title: 'Dress',
+    url: 'https://www.nytimes.com/111',
+    urlToImage: 'https://1111static01.nyt.com/images/',
+  },
+];
+
+describe('Default cards from api', () => {
+  describe('Default Mock api', () => {
+    beforeEach(async () => {
+      render(<SearchBox />);
+      await (axios.get as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({ data: { articles } })
+      );
+    });
+    test('Default api', async () => {
+      expect(axios.get).toHaveBeenCalledWith(
+        'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f5110220c7f6448d84d3250bb882da25'
+      );
     });
   });
 });
