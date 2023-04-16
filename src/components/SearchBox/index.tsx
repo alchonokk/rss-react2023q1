fb66d949cb82ba19633d2a3903dacf858f902285
alchonokk <galunia_suglob@mail.ru>
2023-04-16 18:01:03 +0300

    fix: use store for SearchBox

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,73 +1,74 @@
-import axios from 'axios';
 import { createUrlForEverything, createUrlForSearch } from 'helpers/createSearchUrl';
 import { SearchCard } from 'interfaces';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { CardFromSearch } from './CardFromSearch';
+import { useAppDispatch, useAppSelector } from 'store/hook';
+import {
+  changeValueOfSearch,
+  requestApiAsync,
+  statusAPI,
+  valueAPI,
+  valueSearch,
+} from 'store/reduxSlice';
 
 function SearchBox() {
-  const [searchData, setSearchData] = useState(localStorage.getItem('searchData') || '');
-  const [APIData, setAPIData] = useState<Partial<SearchCard>[]>([]);
-  const [isLoading, setLoading] = useState<boolean>(false);
+  const searchDataStore = useAppSelector(valueSearch);
+  const statusAPIStore = useAppSelector(statusAPI);
+  const dataApiStore = useAppSelector(valueAPI);
+  const [searchData, setSearchData] = useState(searchDataStore);
+  const [isSubmit, setSubmit] = useState<boolean>(false);
   const [textError, setTextError] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const dataNews: Partial<SearchCard>[] = dataApiStore.articles;
 
-  useEffect(() => {
-    const defaultSearchData = localStorage.getItem('searchData');
-    try {
-      axios
-        .get(defaultSearchData ? createUrlForSearch(defaultSearchData) : createUrlForEverything())
-        .then(async (resp) => {
-          const allData = await resp.data;
-          if (resp.status !== 200) {
-            setLoading(false);
-            setTextError(true);
-            return;
-          }
-          setAPIData(allData.articles);
-          setLoading(false);
-          setTextError(false);
-          if (!allData.articles.length) {
-            setTextError(true);
-          }
-        })
-        .catch(() => setTextError(true));
-    } catch (error) {
+  const defaultData = useCallback(async () => {
+    if (!searchDataStore) {
+      setTextError(false);
+      try {
+        const allDatas = await dispatch(
+          requestApiAsync(
+            searchDataStore ? createUrlForSearch(searchDataStore) : createUrlForEverything()
+          )
+        );
+        if (allDatas.payload.status !== 'ok' || !allDatas.payload.articles.length) {
+          setTextError(true);
+          return;
+        }
+        setTextError(false);
+      } catch {
+        setTextError(true);
+      }
+    } else if (searchDataStore && !dataApiStore.articles.length && !isSubmit) {
       setTextError(true);
     }
-  }, []);
+  }, [dataApiStore.articles.length, dispatch, isSubmit, searchDataStore]);
+
+  useEffect(() => {
+    defaultData();
+  }, [defaultData]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchData(e.target.value);
-    if (e.target.value === '') {
-      localStorage.setItem('searchData', '');
-    }
   };
 
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    localStorage.setItem('searchData', searchData);
     e.preventDefault();
-    setLoading(true);
+    setSubmit(true);
+    dispatch(changeValueOfSearch(searchData));
     setTextError(false);
     try {
-      axios
-        .get(searchData ? createUrlForSearch(searchData) : createUrlForEverything())
-        .then(async (resp) => {
-          const allData = await resp.data;
-          if (resp.status !== 200) {
-            setLoading(false);
-            setTextError(true);
-            return;
-          }
-          setAPIData(allData.articles);
-          setLoading(false);
-          setTextError(false);
-          if (!allData.articles.length) {
-            setTextError(true);
-          }
-        })
-        .catch(() => setTextError(true));
-    } catch (error) {
+      const allDatas = await dispatch(
+        requestApiAsync(searchData ? createUrlForSearch(searchData) : createUrlForEverything())
+      );
+      if (allDatas.payload.status !== 'ok' || !allDatas.payload.articles.length) {
+        setTextError(true);
+        return;
+      }
+      setTextError(false);
+    } catch {
       setTextError(true);
     }
+    setSubmit(false);
   };
 
   return (
@@ -84,11 +85,11 @@ function SearchBox() {
         />
       </form>
       <div className="message">
-        {isLoading && <p data-testid="loading">Loading...</p>}
+        {statusAPIStore === 'loading' && <p data-testid="loading">Loading...</p>}
         {textError && <p data-testid="text-error">Some problems with API...or change request</p>}
       </div>
       <main className="block-cards" data-testid="api-card-block">
-        {APIData.map((cardSearch) => {
+        {dataNews.map((cardSearch) => {
           return <CardFromSearch key={cardSearch.url} {...cardSearch}></CardFromSearch>;
         })}
       </main>
